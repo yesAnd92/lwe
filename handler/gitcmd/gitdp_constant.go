902d4caf2c7e39ea4faee1743067a91c1f9f4d92
wangyj <850208971@qq.com>
2023-06-25 10:10:36 +0800

    增加升级工程配置

--- handler/gitcmd/gitdp_constant.go
+++ handler/gitcmd/gitdp_constant.go
@@ -2,8 +2,13 @@ package gitcmd
 
 var pjNameMap = map[string]string{
 	"new-media-rft":         "rft;广播电视",
+	"new-media-rft-api":     "rftapi;广播电视api",
 	"new-media-mp":          "mp;媒体号",
+	"new-media-mp-api":      "mpapi;媒体号",
 	"new-media-mpclient":    "mpclient;媒体号客户端",
+	"new-media-politic":     "mpapi;问政管理",
+	"new-media-politic-api": "politicapi;问政管理api",
+	"new-media-politicunit": "politicunit;问政管理客户端",
 	"new-media-content":     "content;新闻管理",
 	"new-media-content-api": "contentapi;新闻管理api",
 }
