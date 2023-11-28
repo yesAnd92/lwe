ea21b061f85f0a703429ac9051175676ff5adbe4
wangyj <850208971@qq.com>
2023-11-28 16:49:01 +0800

    add pdfc impl

--- handler/pdf/pdf_cut_test.go
+++ handler/pdf/pdf_cut_test.go
@@ -10,14 +10,14 @@ func TestParseCutArg(t *testing.T) {
 	tests := []struct {
 		name    string
 		args    string
-		want    []int
+		want    []string
 		wantErr bool
 	}{
 		// TODO: Add test cases.
 		{
 			name:    "case 1",
 			args:    "1, 2, 4-9,20",
-			want:    []int{1, 2, 4, 5, 6, 7, 8, 9, 20},
+			want:    []string{"1", "2", "4", "5", "6", "7", "8", "9", "20"},
 			wantErr: false,
 		},
 
@@ -31,7 +31,7 @@ func TestParseCutArg(t *testing.T) {
 		{
 			name:    "case 3",
 			args:    "1, ,,,,",
-			want:    []int{1},
+			want:    []string{"1"},
 			wantErr: false,
 		},
 	}
@@ -55,6 +55,7 @@ func TestHandlePdfCut(t *testing.T) {
 		inPdf         string
 		outDir        string
 		selectedPages []string
+		merger        bool
 	}
 	tests := []struct {
 		name    string
@@ -62,10 +63,47 @@ func TestHandlePdfCut(t *testing.T) {
 		wantErr bool
 	}{
 		// TODO: Add test cases.
+		{
+			name: "case 1",
+			args: args{
+				inPdf:         "testdata/zineTest.pdf",
+				outDir:        "testdata/out/",
+				selectedPages: []string{"1", "3", "4", "6", "20"},
+			},
+			wantErr: false,
+		},
+		{
+			name: "no exist outDir",
+			args: args{
+				inPdf:         "testdata/zineTest.pdf",
+				outDir:        "testdata/out/1/",
+				selectedPages: []string{"1", "1", "1"},
+			},
+			wantErr: true,
+		},
+		{
+			name: " wrong outDir",
+			args: args{
+				inPdf:         "testdata/zineTest.pdf",
+				outDir:        "testdata/out/out.pdf",
+				selectedPages: []string{"1", "1", "1"},
+			},
+			wantErr: true,
+		},
+		{
+			name: "merge all pdf",
+			args: args{
+				inPdf:         "testdata/zineTest.pdf",
+				outDir:        "testdata/out/",
+				selectedPages: []string{"1", "3", "5"},
+				merger:        true,
+			},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := HandlePdfCut(tt.args.inPdf, tt.args.outDir, tt.args.selectedPages); (err != nil) != tt.wantErr {
+			if err := HandlePdfCut(tt.args.inPdf, tt.args.outDir, tt.args.selectedPages, tt.args.merger); (err != nil) != tt.wantErr {
 				t.Errorf("HandlePdfCut() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
