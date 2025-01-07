1d58906bfdbd352d9bfddbea0169f7c78c751658
yesAnd <850208971@qq.com>
2025-01-07 17:19:23 +0800

    add the capabilities of AI & implement DeepSeek.

---
+++ ai/deepseek_response.go
@@ -0,0 +1,60 @@
+package ai
+
+type DeepSeekResponse struct {
+	ID                string   `json:"id"`
+	Choices           []Choice `json:"choices"`
+	Created           int64    `json:"created"`
+	Model             string   `json:"model"`
+	SystemFingerprint string   `json:"system_fingerprint"`
+	Object            string   `json:"object"`
+	Usage             Usage    `json:"usage"`
+}
+
+type Choice struct {
+	FinishReason string   `json:"finish_reason"`
+	Index        int      `json:"index"`
+	Message      Message  `json:"message"`
+	Logprobs     Logprobs `json:"logprobs"`
+}
+
+type Message struct {
+	Content   string     `json:"content"`
+	ToolCalls []ToolCall `json:"tool_calls"`
+	Role      string     `json:"role"`
+}
+
+type ToolCall struct {
+	ID       string   `json:"id"`
+	Type     string   `json:"type"`
+	Function Function `json:"function"`
+}
+
+type Function struct {
+	Name      string `json:"name"`
+	Arguments string `json:"arguments"`
+}
+
+type Logprobs struct {
+	Content []ContentItem `json:"content"`
+}
+
+type ContentItem struct {
+	Token       string       `json:"token"`
+	Logprob     float64      `json:"logprob"`
+	Bytes       []int        `json:"bytes"`
+	TopLogprobs []TopLogprob `json:"top_logprobs"`
+}
+
+type TopLogprob struct {
+	Token   string  `json:"token"`
+	Logprob float64 `json:"logprob"`
+	Bytes   []int   `json:"bytes"`
+}
+
+type Usage struct {
+	CompletionTokens      int64 `json:"completion_tokens"`
+	PromptTokens          int64 `json:"prompt_tokens"`
+	PromptCacheHitTokens  int64 `json:"prompt_cache_hit_tokens"`
+	PromptCacheMissTokens int64 `json:"prompt_cache_miss_tokens"`
+	TotalTokens           int64 `json:"total_tokens"`
+}
